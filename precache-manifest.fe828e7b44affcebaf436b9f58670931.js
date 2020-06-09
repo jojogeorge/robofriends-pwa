f35b848549a40161b043296d99890c8fea1ec9a4
George <jojgeorge@deloitte.com>
2020-06-09 21:15:49 +0530

    Updates

---
+++ precache-manifest.fe828e7b44affcebaf436b9f58670931.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37933a1f57945bb3f532017a5311eab4",
+    "url": "/robofriends-pwa/index.html"
+  },
+  {
+    "revision": "0fbc3c95538adacc3480",
+    "url": "/robofriends-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5f67a51e293bab079617",
+    "url": "/robofriends-pwa/static/css/main.4fe2c603.chunk.css"
+  },
+  {
+    "revision": "0fbc3c95538adacc3480",
+    "url": "/robofriends-pwa/static/js/2.c2073658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends-pwa/static/js/2.c2073658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f67a51e293bab079617",
+    "url": "/robofriends-pwa/static/js/main.ca48d724.chunk.js"
+  },
+  {
+    "revision": "114f846ec09b7002c39b",
+    "url": "/robofriends-pwa/static/js/runtime-main.8a8199fb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
